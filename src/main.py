a0c98034e193e71c503e998f337934e4ea2aac40
jip-van-sommeren <jip.van.sommeren@gmail.com>
2025-02-20 16:19:10 +0100

    actions zou nu moeten werken, laatste push naar main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,23 @@
 import os
 import json
+import psycopg2
+from psycopg2.extras import RealDictCursor
+
+conn = None
+
+
+def get_db_connection():
+    global conn
+
+    if conn is None:
+        conn = psycopg2.connect(
+            host=os.environ.get("DB_HOST"),
+            port=os.environ.get("DB_PORT", 5432),
+            database=os.environ.get("DB_NAME"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD"),
+        )
+    return conn
 
 
 def lambda_handler(event, context):
